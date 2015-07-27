8d19e2afa5c989f65235fcfdf1498bb638d14869
Rayzworld <kaelkalo2015@gmail.com>
2015-07-27 18:20:56 +0800

    Fixes UI

--- RKTabView/RKTabView.m
+++ RKTabView/RKTabView.m
@@ -273,7 +273,7 @@
     //Subviews frames
     if (titleLabel) {
         CGFloat wholeGapHeight = tab.bounds.size.height - ((UIView *)interfaceElement).bounds.size.height - titleSize.height;
-        titleLabel.frame = CGRectMake(tab.bounds.size.width/2 - titleSize.width/2, wholeGapHeight*2/3+((UIView *)interfaceElement).bounds.size.height, titleSize.width+2, titleSize.height+2);
+        titleLabel.frame = CGRectMake(tab.bounds.size.width/2 - titleSize.width/2, wholeGapHeight*2/3+((UIView *)interfaceElement).bounds.size.height-5, titleSize.width+2, titleSize.height+2);
         ((UIView *)interfaceElement).frame = CGRectMake(tab.bounds.size.width/2 - ((UIView *)interfaceElement).bounds.size.width/2, wholeGapHeight/3, ((UIView *)interfaceElement).bounds.size.width, ((UIView *)interfaceElement).bounds.size.height);
         [tab addSubview:titleLabel];
     } else {
